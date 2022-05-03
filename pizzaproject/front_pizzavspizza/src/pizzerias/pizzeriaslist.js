1bb043c083951d3451f0f401c27cc7e20d4b7141
VincentGichohi <vincentegichohi@gmail.com>
2022-05-03 17:40:54 +0300

    Add an absolute url

--- pizzaproject/front_pizzavspizza/src/pizzerias/pizzeriaslist.js
+++ pizzaproject/front_pizzavspizza/src/pizzerias/pizzeriaslist.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import PizzaDetail from './pizzeriadetail';
-import DummyData from "./dummydata.json";
-import axios from './axios';
+import axios from 'axios';
 class PizzaList extends React.Component {
+
+    state = {
+        pizzeriaData: [],
+    }
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8080/')
+        .then((response) => {
+            this.setState({pizzeriasData: response.data})
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+    }
     render() {
         return (
             <div>
-                {DummyData.map(item => {
-                    return < PizzaDetail p = {item} />
-                })
-                }
+            {this.state.pizzeriasData.map( item => {
+                return <h3 key={item.id}>{item.pizzeria_name}, {item.city}</h3>
+            })}
             </div>
         )
     }
